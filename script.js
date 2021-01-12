81fe1d8a64655e3fa6f135d7b5c46a23c114864d
Indervir Singh <iindervirsingh@gmail.com>
2021-01-11 16:31:13 -0800

    fixed loop for error handling character type

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function generatePassword() {
     var passwordNotWithinBounds = true;
     while (passwordNotWithinBounds) {
 
-        // Store the user input
+        // Store the user input for password length
         var passwordLength = parseInt(prompt('How many chars?'));
 
         // Check if it meets our length requirement
@@ -35,33 +35,39 @@ function generatePassword() {
         };
     }
 
+    alert("You will be asked " + passwordLength + " prompts to determine the password.");
     // This loop: asks the user for the char type at each iteration and updates the password accordingly
     for (let i = 0; i < passwordLength; i++) {
-        alert("You will be asked " + passwordLength + " prompts to determine the password.");
+        
+        // Store user input for character type
         var currentCharacter = prompt("Please enter the type of character for the password: 'c' for capital letter, 'l' for lowercase letter, 'n' for a number, 's' for a special character");
+        alert(typeof currentCharacter);
 
         // This loop is for: error handling password character type
         var incorrectCharacterType = true;
         while (incorrectCharacterType) {
-            if (currentCharacter != 'c' || currentCharacter != 'l' || currentCharacter != 'n' || currentCharacter != 's') {
-                alert("PLEASE ENTER VALUES 'c', 'l', 'n', 's' ONLY");
-            } else {
+            // If user selects correct type, then break error handling loop
+            if ( (currentCharacter == "c") || (currentCharacter == "l") || (currentCharacter == "n") || (currentCharacter == "s") ) {
                 incorrectCharacterType = false;
+            } else {
+                alert("PLEASE ENTER VALUES 'c', 'l', 'n', 's' ONLY");
+                currentCharacter = prompt("Please enter the type of character for the password: 'c' for capital letter, 'l' for lowercase letter, 'n' for a number, 's' for a special character");
             }
 
-            currentCharacter = prompt("Please enter the type of character for the password: 'c' for capital letter, 'l' for lowercase letter, 'n' for a number, 's' for a special character");
         }
+
+        // This switch case: generates a random character depending on the user selected current-character-type, then appends it to the password
         switch (currentCharacter) {
-            case 'c':
+            case "c":
                 password += capitalLetters.charAt(Math.floor(Math.random() * capitalLetters.length));
                 break;
-            case 'l':
+            case "l":
                 password += lowercaseLetters.charAt(Math.floor(Math.random() * lowercaseLetters.length));
                 break;
-            case 'n':
+            case "n":
                 password += numbers.charAt(Math.floor(Math.random() * numbers.length));
                 break;
-            case 's':
+            case "s":
                 password += specialCharacters.charAt(Math.floor(Math.random() * specialCharacters.length));
                 break;
         }
